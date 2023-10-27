f287253e93f3e2b1443ac6fab0c722360523a0c2
keshri522 <rkeshri522@gmail.com>
2023-10-27 11:54:04 +0530

    added  submit button work if there are any error present

--- script.js
+++ script.js
@@ -19,7 +19,11 @@ const handleSubmit = (e) => {
     password,
     cpassword,
   };
-  ValidateInputs();
+  // for making our submit button work only if there are no error message
+  const ErrorMessage = ValidateInputs();
+  if (ErrorMessage.length === 0) {
+    // then only we call a api here
+  }
 };
 // this is just for email matching patterns using regex
 const emailregex = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
@@ -50,42 +54,51 @@ const ValidateInputs = () => {
   const Password = password.trim();
   const Cpassword = cpassword.trim();
 
+  // for thee error message
+  const ErrorMessage = [];
   if (FirstName === "") {
     Seterror(document.querySelector("#name"), "First Name is required!");
+    ErrorMessage.push("First Name is required!");
   } else {
     SetSuccess(document.querySelector("#name"));
   }
 
   if (LastName === "") {
     Seterror(document.querySelector("#last_name"), "Last Name is required!");
+    ErrorMessage.push("Last Name is required!");
   } else {
     SetSuccess(document.querySelector("#last_name"));
   }
 
   if (Username === "") {
     Seterror(document.querySelector("#username"), "Username is required!");
+    ErrorMessage.push("UserName is required!");
   } else {
     SetSuccess(document.querySelector("#username"));
   }
 
   if (!emailregex.test(email)) {
     Seterror(document.querySelector("#email"), "Email format not matched!");
+    ErrorMessage.push("Email is required!");
   } else {
     SetSuccess(document.querySelector("#email"));
   }
 
   if (Password === "") {
     Seterror(document.querySelector("#password"), "Password is required!");
+    ErrorMessage.push("Password is required!");
   } else if (Password.length < 2) {
     Seterror(
       document.querySelector("#password"),
       "Password must be at least two characters!"
     );
+    ErrorMessage.push("Password is required!");
   } else if (Password.length > 10) {
     Seterror(
       document.querySelector("#password"),
       "Password must be less than ten characters!"
     );
+    ErrorMessage.push("Password is required!");
   } else {
     SetSuccess(document.querySelector("#password"));
   }
@@ -95,11 +108,14 @@ const ValidateInputs = () => {
       document.querySelector("#cpassword"),
       "Confirm password is required!"
     );
+    ErrorMessage.push("Cpassword is required!");
   } else if (Cpassword !== Password) {
     Seterror(document.querySelector("#cpassword"), "Password does not match!");
+    ErrorMessage.push("Cpassword is required!");
   } else {
     SetSuccess(document.querySelector("#cpassword"));
   }
+  return ErrorMessage;
 };
 
 form.addEventListener("submit", handleSubmit);
